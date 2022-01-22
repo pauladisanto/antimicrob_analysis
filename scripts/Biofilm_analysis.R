c120d4e6304e6714e7ad2b08e7c9bc49d435bb58
Paula Di Santo <unlpbiotec@gmail.com>
2022-01-22 19:06:55 +0100

    First push of the scripts

---
+++ scripts/Biofilm_analysis.R
@@ -0,0 +1,313 @@
+#script for biofilm formation with an imput table with 4 conditions instead 4 conditions and the last file is negative control (only Acectic Acid)
+ 
+# We read the raw data from the file, skipping the first 14 rows which contain irrelevant information
+#source("script_biofilm_formation.R")
+#filename = "Absorbancia550nm_ensayoBF_MH_48h_02_y_2_glucosa.csv"
+
+#raw_data = read.table(filename, skip=14, header=FALSE, sep="\t")
+
+
+process_table <- function(filename) {  
+	# This function processes a table to obtain a curated dataframe
+
+	# We read the raw data from the file, skipping the first 14 rows which contain irrelevant information
+	raw_data = read.table(filename, skip=14, header=FALSE, sep="\t")
+
+	# Now we construct a new data frame with the good data for file 1
+	# For this, we remove the odd columns and rows which contain dummy data
+
+}
+
+filename1 = "Absorbancia550nm_ensayoBF_MH_24h_02_y_2_glucosa.csv"
+filename2 = "Absorbancia550nm_ensayoBF_MH_48h_02_y_2_glucosa.csv"
+
+# process the tables
+raw_data1 = process_table(filename1)
+raw_data2 = process_table(filename2)
+
+# merge two data frames into one
+compounds_merged <- rbind(raw_data1,raw_data2)
+
+
+#===================================================================================================
+
+#Define the column names
+column_names = c('X','A', 'B', 'C', 'D','E', 'F','G','H','I','J','K','L')
+colnames(compounds_merged) = column_names
+
+#to remove the first column
+data <- subset( compounds_merged, select = -c(X))
+
+# We have n_m measurements for each of the n_c compounds
+n_m = 3 
+n_b = 12    # amount of wells for each bacteria
+n_row = 16
+n_col = 12
+n_c = n_row * (n_col/n_m)
+
+# Create a new dataframe separating the measurements for each compound
+# The first column has the compound name, and the second column is the measurement
+compounds = data.frame()
+sum = 0
+for(i in 1:n_row) {
+	for(j in 1:n_col) {
+		sum = sum + 1
+
+		# Obtain the index "i_c" for each compound
+		i_c = as.integer( (sum-1)/n_m ) + 1
+		j_c = (j-1)%%3 + 1
+		name_c = paste('Condition', sprintf("%02d", i_c), sep = "_")
+		compounds[n_m*(i_c-1)+j_c,1] = name_c
+		compounds[n_m*(i_c-1)+j_c,2] = data[i,j]
+
+		# Obtain the index "i_b" for each bacteria
+		i_b = as.integer( (sum-1)/n_b ) + 1
+		name_b = paste('bacteria', as.character(i_b), sep = "_")
+		compounds[n_m*(i_c-1)+j_c,3] = name_b
+	}
+}
+
+
+#===============================================================================================================
+
+#bacteria 24 h incubation
+#bacteria_condition =  c("EntC 24h 30C 0.2GLU", "EntC 24h 37C 0.2GLU", "EntC 24h 30C 2GLU", "EntC 24h 37C 2GLU",
+#											"StaphE 24h 30C 0.2GLU", "StaphE 24h 37C 0.2GLU", "StaphE 24h 30C 2GLU", "StaphE 24h 37C 2GLU",
+#											"EColi 24h 30C 0.2GLU", "EColi 24h 37C 0.2GLU", "EColi 24h 30C 2GLU", "EColi 24h 37C 2GLU",
+#											"Bsub 24h 30C 0.2GLU", "Bsub 24h 37C 0.2GLU", "Bsub 24h 30C 2GLU", "Bsub 24h 37C 2GLU",
+#											"Pput 24h 30C 0.2GLU", "Pput 24h 37C 0.2GLU", "Pput 24h 30C 2GLU", "Pput 24h 37C 2GLU",
+#										  "PC 24h 30C 0.2GLU", "PC 24h 37C 0.2GLU", "PC 24h 30C 2GLU", "PC 24h 37C 2GLU",
+#										  "PB 24h 30C 0.2GLU", "PB 24h 37C 0.2GLU", "PB 24h 30C 2GLU", "PB 24h 37C 2GLU",
+#										  "NEG", "NEG", "NEG", "NEG")
+
+
+#bacteria 48 h incubation
+#bacteria_condition =  c("EntC 48h 30C 0.2GLU", "EntC 48h 37C 0.2GLU", "EntC 48h 30C 2GLU", "EntC 48h 37C 2GLU",
+#											"StaphE 48h 30C 0.2GLU", "StaphE 48h 37C 0.2GLU", "StaphE 48h 30C 2GLU", "StaphE 48h 37C 2GLU",
+#											"EColi 48h 30C 0.2GLU", "EColi 48h 37C 0.2GLU", "EColi 48h 30C 2GLU", "EColi 48h 37C 2GLU",
+#											"Bsub 48h 30C 0.2GLU", "Bsub 48h 37C 0.2GLU", "Bsub 48h 30C 2GLU", "Bsub 48h 37C 2GLU",
+#											"Pput 48h 30C 0.2GLU", "Pput 48h 37C 0.2GLU", "Pput 48h 30C 2GLU", "Pput 48h 37C 2GLU",
+#										  "PC 48h 30C 0.2GLU", "PC 48h 37C 0.2GLU", "PC 48h 30C 2GLU", "PC 48h 37C 2GLU",
+#										  "PB 48h 30C 0.2GLU", "PB 48h 37C 0.2GLU", "PB 48h 30C 2GLU", "PB 48h 37C 2GLU",
+#										  "NEG", "NEG", "NEG", "NEG")
+
+
+bacteria_condition =  c("EntC 24h 30C 0.2GLU", "EntC 24h 37C 0.2GLU", "EntC 24h 30C 2GLU", "EntC 24h 37C 2GLU",
+											"StaphE 24h 30C 0.2GLU", "StaphE 24h 37C 0.2GLU", "StaphE 24h 30C 2GLU", "StaphE 24h 37C 2GLU",
+											"EColi 24h 30C 0.2GLU", "EColi 24h 37C 0.2GLU", "EColi 24h 30C 2GLU", "EColi 24h 37C 2GLU",
+											"Bsub 24h 30C 0.2GLU", "Bsub 24h 37C 0.2GLU", "Bsub 24h 30C 2GLU", "Bsub 24h 37C 2GLU",
+											"Pput 24h 30C 0.2GLU", "Pput 24h 37C 0.2GLU", "Pput 24h 30C 2GLU", "Pput 24h 37C 2GLU",
+										  "PC 24h 30C 0.2GLU", "PC 24h 37C 0.2GLU", "PC 24h 30C 2GLU", "PC 24h 37C 2GLU",
+										  "PB 24h 30C 0.2GLU", "PB 24h 37C 0.2GLU", "PB 24h 30C 2GLU", "PB 24h 37C 2GLU",
+										  "NEG", "NEG", "NEG", "NEG","EntC 48h 30C 0.2GLU", "EntC 48h 37C 0.2GLU", "EntC 48h 30C 2GLU", "EntC 48h 37C 2GLU",
+											"StaphE 48h 30C 0.2GLU", "StaphE 48h 37C 0.2GLU", "StaphE 48h 30C 2GLU", "StaphE 48h 37C 2GLU",
+											"EColi 48h 30C 0.2GLU", "EColi 48h 37C 0.2GLU", "EColi 48h 30C 2GLU", "EColi 48h 37C 2GLU",
+											"Bsub 48h 30C 0.2GLU", "Bsub 48h 37C 0.2GLU", "Bsub 48h 30C 2GLU", "Bsub 48h 37C 2GLU",
+											"Pput 48h 30C 0.2GLU", "Pput 48h 37C 0.2GLU", "Pput 48h 30C 2GLU", "Pput 48h 37C 2GLU",
+										  "PC 48h 30C 0.2GLU", "PC 48h 37C 0.2GLU", "PC 48h 30C 2GLU", "PC 48h 37C 2GLU",
+										  "PB 48h 30C 0.2GLU", "PB 48h 37C 0.2GLU", "PB 48h 30C 2GLU", "PB 48h 37C 2GLU",
+										  "NEG", "NEG", "NEG", "NEG")
+
+
+for(i in 1:n_c) {
+	# Retrieve a string as "Condition_i" for each "i"
+	name_c = paste('Condition', sprintf("%02d", i), sep = "_")
+
+	# Replace when the compounds has this name
+	compounds[compounds == name_c] <- bacteria_condition[i]
+}
+
+
+# Add a header
+column_names = c('Condition', 'Absorbance', 'Bacteria')
+colnames(compounds) = column_names
+
+# select blanks and calculate the media to rest the value to the crystal violect data
+negative_control <- subset(compounds, Bacteria == "bacteria_8" | Bacteria == "bacteria_16" , select=c(Condition, Absorbance, Bacteria))
+
+mean_negative_control <- tapply(negative_control$Absorbance,factor(negative_control$Condition),mean)
+
+compounds<-transform(compounds,Absorbance=Absorbance-mean_negative_control)
+
+
+#=========================================subselect tables====================
+#df["Column Name"][df["Column Name"] == "Old Value"] <- "New Value"
+
+#DEFINIR LAS BACTERIAS DE 9 A 16 COMO SUS PARES DE 1 A 8
+compounds["Bacteria"][compounds["Bacteria"] == "bacteria_9"]  <- "bacteria_1"
+compounds["Bacteria"][compounds["Bacteria"] == "bacteria_10"]  <- "bacteria_2"
+compounds["Bacteria"][compounds["Bacteria"] == "bacteria_11"]  <- "bacteria_3"
+compounds["Bacteria"][compounds["Bacteria"] == "bacteria_12"]  <- "bacteria_4"
+compounds["Bacteria"][compounds["Bacteria"] == "bacteria_13"]  <- "bacteria_5"
+compounds["Bacteria"][compounds["Bacteria"] == "bacteria_14"]  <- "bacteria_6"
+compounds["Bacteria"][compounds["Bacteria"] == "bacteria_15"]  <- "bacteria_7"
+compounds["Bacteria"][compounds["Bacteria"] == "bacteria_16"]  <- "bacteria_8"
+
+
+# Make a list of all the dataframes. The element "i" of the list is the df for the "i" bacteria.
+dfs = list()
+for(i in 1:n_row) {
+	dfs[[i]] <- subset(compounds, Bacteria == paste("bacteria", i, sep = "_"), select=c(Condition, Absorbance, Bacteria))
+}
+
+
+
+#============================================post-test==============================================
+
+library(agricolae)
+
+aovScreening1<-aov(Absorbance~Condition,data=dfs[[1]])
+summary(aovScreening1)
+
+postestScreening1<-LSD.test(aovScreening1,"Condition")
+print(postestScreening1)
+
+#=========================================
+
+aovScreening2<-aov(Absorbance~Condition,data=dfs[[2]])
+summary(aovScreening2)
+
+postestScreening2<-LSD.test(aovScreening2,"Condition")
+print(postestScreening2)
+
+#=========================================
+
+aovScreening3<-aov(Absorbance~Condition,data=dfs[[3]])
+summary(aovScreening3)
+
+postestScreening3<-LSD.test(aovScreening3,"Condition")
+print(postestScreening3)
+
+#=========================================
+
+aovScreening4<-aov(Absorbance~Condition,data=dfs[[4]])
+summary(aovScreening4)
+
+postestScreening4<-LSD.test(aovScreening4,"Condition")
+print(postestScreening4)
+
+#=========================================
+
+aovScreening5<-aov(Absorbance~Condition,data=dfs[[5]])
+summary(aovScreening5)
+
+postestScreening5<-LSD.test(aovScreening5,"Condition")
+print(postestScreening5)
+
+#=========================================
+
+aovScreening6<-aov(Absorbance~Condition,data=dfs[[6]])
+summary(aovScreening6)
+
+postestScreening6<-LSD.test(aovScreening6,"Condition")
+print(postestScreening6)
+
+#=========================================
+
+aovScreening7<-aov(Absorbance~Condition,data=dfs[[7]])
+summary(aovScreening7)
+
+postestScreening7<-LSD.test(aovScreening7,"Condition")
+print(postestScreening7)
+
+#=========================================
+
+# CALCULATIONS TO MAKE THE PLOT
+
+#=========================================================================
+# Do all conditions in one loop 
+
+mean_conditions = list()
+sd_conditions = list()
+conditions = list()
+dfs_plot = list() 
+for(i in 1:7){
+	mean_conditions[[i]] <- tapply(dfs[[i]]$Absorbance,factor(dfs[[i]]$Condition),mean)
+	sd_conditions[[i]] <- tapply(dfs[[i]]$Absorbance,factor(dfs[[i]]$Condition),sd)
+	conditions[[i]] = names(mean_conditions[[i]])
+	dfs_plot[[i]] = data.frame(
+		"Cond" = conditions[[i]],
+		"Mean" = mean_conditions[[i]], 
+		"SD" = sd_conditions[[i]]
+		)
+}
+
+
+#=========================================================================
+
+library("ggplot2")
+
+
+
+
+colors = c(1,2,3,4,5,6,7) #TO DO: DEFINE NICE COLOURS FOR FILLING!
+titles = c("Enterococcus raffinosus", 
+	"Staphylococcus epidermidis", "Escherichia coli", "Bacillus subtilis",  "Pseudomonas Putida",  "Pectobacterium carotovorum", 
+	"Paraburkholderia caledonica")
+
+significances = list()
+significances[[1]] = c("c", "bc", "bc", "a", "c", "bc", "c", "b")
+significances[[2]] = c("d", "cd","e", "c", "a", "a", "b", "a")
+significances[[3]] = c("d", "d", "b", "d", "c", "c", "a", "c")
+significances[[4]] = c("g", "c", "fg", "de", "a", "b", "cd", "ef")
+significances[[5]] = c("bc", "abc", "c", "a", "abc", "bc", "ab", "ab")
+significances[[6]] = c("c", "ab", "bc", "c", "a", "bc", "bc", "bc")
+significances[[7]] = c("b", "b", "b", "a", "b", "b", "b", "b")
+
+
+distance = list()
+distance[[1]] = c(0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01)
+distance[[2]] = c(0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2)
+distance[[3]] = c(0.03, 0.03, 0.03, 0.03, 0.03, 0.03, 0.03, 0.03)
+distance[[4]] = c(0.03, 0.03, 0.03, 0.03, 0.03, 0.03, 0.03, 0.03)
+distance[[5]] = c(0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05)
+distance[[6]] = c(0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005)
+distance[[7]] = c(0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01)
+distance[[8]] = c(0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01)
+
+
+
+
+
+# TO DO> CHANGE 7 for n_b
+label = c()
+for (i in 1:7){
+	Mean_i = dfs_plot[[i]]$Mean
+	SD_i = dfs_plot[[i]]$SD
+  label = significances[[i]]
+  distances = distance[[i]]
+	p <- ggplot(data=dfs_plot[[i]], aes(x=Cond, y=Mean)) + 
+	  					geom_bar(stat="identity", fill=i)+ 
+	  					geom_errorbar(aes(ymin=Mean_i-SD_i, ymax=Mean_i+SD_i), width=.2,
+	      			position=position_dodge(0.05)) + 
+	      			theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=0.8)) +
+	      			theme(legend.position="top") + 
+	            labs(title=element_blank(), x="Condition", y = "Absorbance")+
+	            	annotate("text", x = 1, y = Mean_i[1]+SD_i[1]+distances[1], label = label[1],
+	    						size = 7, family = "Times", fontface = "bold.italic", colour = "black")+
+	    					annotate("text", x = 2, y = Mean_i[2]+SD_i[2]+distances[2], label = label[2],
+	    		    		size = 7, family = "Times", fontface = "bold.italic", colour = "black")+
+	    					annotate("text", x = 3, y = Mean_i[3]+SD_i[3]+distances[3], label = label[3],
+	    						size = 7, family = "Times", fontface = "bold.italic", colour = "black")+
+	    					annotate("text", x = 4, y = Mean_i[4]+SD_i[4]+distances[4], label = label[4],
+	    						size = 7, family = "Times", fontface = "bold.italic", colour = "black")+
+	    					annotate("text", x = 5, y = Mean_i[5]+SD_i[5]+distances[5], label = label[5],
+	    						size = 7, family = "Times", fontface = "bold.italic", colour = "black")+
+	    					annotate("text", x = 6, y = Mean_i[6]+SD_i[6]+distances[6], label = label[6],
+	    						size = 7, family = "Times", fontface = "bold.italic", colour = "black")+
+	    					annotate("text", x = 7, y = Mean_i[7]+SD_i[7]+distances[7], label = label[7],
+	    						size = 7, family = "Times", fontface = "bold.italic", colour = "black")+
+	    					annotate("text", x = 8, y = Mean_i[8]+SD_i[8]+distances[8], label = label[8],
+	    						size = 7, family = "Times", fontface = "bold.italic", colour = "black")+
+	              ggtitle(titles[i])
+
+	print(p)
+	name_plot = paste( paste("plot_biofilm", i, sep="_"), ".jpg", sep="")
+	ggsave(name_plot)
+}
+
+
+
+
+
+
---
+++ scripts/Hit_findind_assay.R
@@ -0,0 +1,189 @@
+#compound_numbers1 = c(, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , )
+
+
+library("ggplot2")
+
+verbose = FALSE # Set to TRUE to print stuff on the terminal
+
+# I am using 79 and 80 for positive and negative controls, respectively (MUST BE CONSISTENT WITH FUNCTION number_to_name)
+
+filename1 = "Hit_assay_samples_72_to_21_calibrated_equipment_bottom_light_BSubtillis.csv"
+# 41 is bad, assume it is a negative control (80) to filter it
+compound_numbers1 = c(80, 1, 9, 17, 80, 2, 10, 18, 80, 3, 11, 19, 80, 4, 12, 20, 80, 5, 13, 21, 80, 6, 14, 79, 80, 7, 15, 79, 80, 8, 16, 80)
+df1 = process_table(filename1, compound_numbers1, verbose)
+
+filename2 = "Hit_assay_samples_22_to_50_calibrated_equipment_bottom_light_BSubt.csv" #cambio 31 por 80 porque el valor es malo
+# 22 and 23 are bad, assume they are negative controls (80) to filter them
+compound_numbers2 = c(22, 30, 38, 46, 23, 80, 39, 47, 24, 32, 40, 48, 25, 33, 41, 49, 26, 34, 42, 50, 27, 35, 43, 79, 28, 36, 44, 79, 29, 37, 45, 80)
+df2 = process_table(filename2, compound_numbers2, verbose)
+
+filename3 = "Hit_assay_samples_51_to_78_calibrated_equipment_bottom_light_Bsubt.csv" 
+compound_numbers3 = c(51, 59, 67, 75, 52, 60, 68, 76, 53, 61, 69, 77, 54, 62, 70, 78, 55, 63, 71, 31, 56, 64, 72, 79, 57, 65, 73, 79, 58, 66, 74, 80)
+df3 = process_table(filename3, compound_numbers3, verbose)
+
+
+#filename4 = "Hit_assay_samples_72_to_21_calibrated_equipment_bottom_light_Ecoli.csv"
+#compound_numbers4 = c(72, 80, 80, 80, 73, 80, 80, 80, 74, 80, 80, 80, 75, 80, 80, 80, 76, 80, 80, 80, 77, 80, 80, 80, 78, 80, 80, 80, 79, 80, 80, 80)
+#df4 = process_table(filename4, compound_numbers4, verbose)
+
+
+# Add all the dataframes into a single one for plotting
+df <- rbind(df1, df2, df3)
+
+df_ordered = df[order(df$Mean),]
+
+
+# Make the barplot
+p <- ggplot(data=df, aes(x=reorder(Comp, Mean), y=Mean)) +
+  geom_bar(stat="identity")+
+  geom_errorbar(aes(ymin=Mean-SD, ymax=Mean+SD), width=.2,
+                 position=position_dodge(0.05)) + 
+                 theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=0.8)) +
+                 theme(legend.position="top") + 
+                 labs(title=element_blank(), x="Compound", y = "Relative Fluorescence")+
+                 ggtitle("B. Subtilis")
+
+
+p + xlab("B. subtilis")
+
+print(p)
+
+
+ggsave("plot_compuestos_BSub.jpg")
+
+
+ggsave("plot_compuestos_BSub.pdf")
+
+#==============================================================================================================================
+#
+# FUNCTIONS
+#
+#==============================================================================================================================
+
+number_to_name <- function(n) {
+	# n is the number of the internal label
+	names = c('KLG-4P', 'KLG-13A', 'KLG-9D', 'KLG-6M', 'KLG-12R', 'KLG-12T', 'KLG-11F', 'KLG-6I', 'KLG-9M', 'JK-8H', 'KLG-10B', 'KLG-14F', 'KLG-12J', 'KLG-6P', 'KLG-4A (CRUDE)', 'JM-3MTS', 'JM-4MTS', 'JM-2MTR', 'JM-23MTL', 'JM-11MLS', 'JM-11MTL', 'JM-12MTL', 'JM-4MFL', 'JM-6MFL', 'JM-1MFL', 'JM-9MIS', 'JM-16MIS', 'JM-5AMLS', 'JM-5MTL', 'JM-1BMLS', 'JM-8MIS', 'JM-15MIS', 'JM-MTR', 'JM-MIS', 'JM-MIR', 'JM-MFS', 'JM-MTL', 'JM-MIL', 'JM-MFR', 'JM-MFL', 'JM-MTS', 'MAN-SML-9596A', 'MAN-SML-105I', 'MAN-SML-105F', 'MAN-PES-B13', 'MAN-SML-105C', 'MAN-SMR-D180a', 'MAN-SML-110I', 'MAN-SMR-D75-78C23A', 'MAN-SMR-D108c', 'MAN-SML-117A', 'MAN-SMR-D8284CE12', 'MAN-SMR-D101H', 'MAN-SMR-D1416Y', 'MAN-SML-110M', 'MAN-SMR-D7C', 'MAN-SMR-D8B2', 'PERE', 'PELE', 'PESE', 'SMLE', 'SMLE-SMLE', 'MAN-SML-6465A (ACN LC-M5)', 'CLL', 'CKL', 'CLS', 'CKS', 'CKR', 'CLR', 'SMRE', 'KVBA-38B', 'KVBA-38A', 'KVBA-38C', 'KVPA-36C', 'KVPA-36L', 'KVBA-38D', 'KVBA-38F', 'NKR-2B', 'POS', 'NEG')
+	return(names[n])
+}
+
+
+process_table <- function(filename, compound_numbers, verbose) {  
+  # This function reads a table and returns a dataframe of compound names, means and standard deviations
+
+  # Check that the argument is correct
+  if (length(compound_numbers) < 32){print("ERROR! Missing compound numbers")}
+  if (length(compound_numbers) > 32){print("ERROR! Too many compound numbers")}
+
+	# We read the raw data from the file, skipping the first 14 rows which contain irrelevant information
+	raw_data = read.table(filename,	skip=14, header=FALSE, sep="\t")
+
+	# Now we construct a new data frame with the good data
+	# For this, we remove the odd columns and rows which contain dummy data
+
+	# Define the dimension of the cured dataset
+	n_row = nrow(raw_data) / 2 
+	n_col = (ncol(raw_data) - 1) / 2
+	data = data.frame()
+	for(i in 1:n_row) {
+		for(j in 1:n_col) { 
+			data[i,j] = raw_data[2*i-1, 2*j]
+		}
+	}
+
+	if(verbose){print(data)}
+
+
+	# We have n_m measurements for each of the n_c compounds
+	n_m = 3 
+	n_c = n_row * (n_col/n_m)
+
+	# Create a new dataframe separating the measurements for each compound
+	# The first column has the compound name, and the second column is the measurement
+	compounds = data.frame()
+	sum = 0
+	for(i in 1:n_row) {
+		for(j in 1:n_col) {
+			# Obtain the index "i_c" for each compound
+			sum = sum + 1
+			i_c = as.integer( (sum-1)/3 ) + 1 # this truncates the number (no decimals)
+			j_c = (j-1)%%3 + 1 # assign the same value three times
+			compounds[n_m*(i_c-1)+j_c,1] = number_to_name(compound_numbers[i_c]) # name of the first column  
+			compounds[n_m*(i_c-1)+j_c,2] = data[i,j]
+		}
+	}
+
+
+	if(verbose){print(compounds)}
+
+
+	# Add a header with the column names
+	colnames(compounds) = c('Compound', 'Fluorescence')
+
+	# Filter small values because maybe the plate is not full
+	min_fluo = 1000
+	Compounds_filter <- subset(compounds, Fluorescence >= min_fluo, select=c(Compound, Fluorescence))
+
+	# Calculate the mean and standard deviation
+	mean_compounds <- tapply(Compounds_filter$Fluorescence,factor(Compounds_filter$Compound),mean)
+	sd_compounds <- tapply(Compounds_filter$Fluorescence,factor(Compounds_filter$Compound),sd)
+
+	# Normalize to the positive control and remove both controls from the list
+	# Also store the name of the compounds that will be plotted
+	mean_control_neg = mean_compounds["NEG"]
+	mean_control_pos = mean_compounds["POS"] 
+	mean_compounds_ratio = c()
+	sd_compounds_ratio = c()
+	name_compounds_plot = c()
+	i = 0
+	index_name = 0
+	for(mean in mean_compounds){
+		i = i + 1
+		index_name = index_name + 1
+		if(mean != mean_control_neg && mean != mean_control_pos){
+			mean_compounds_ratio[i] = mean/mean_control_pos
+			sd_compounds_ratio[i] = sd_compounds[i]/mean_control_pos
+			name_compounds_plot[i] = names(mean_compounds)[index_name]} else i = i-1
+	}
+
+	# Create a dataframe and return it
+	df = data.frame(
+		"Comp"=name_compounds_plot,
+		"Mean"=mean_compounds_ratio, 
+		"SD"=sd_compounds_ratio
+	)
+	if(verbose){print(df)}
+
+	return(df)
+}
+
+
+
+
+
+
+#===================================================================================================================
+# ANOTHER VERSION
+
+
+# Create a new dataframe in which each compound is a row
+# The first column has the compound name
+# The following columns store the different measurements for a given compound
+
+compounds = data.frame()
+sum = 0
+for(i in 1:n_row) {
+	for(j in 1:n_col) {
+		# Obtain the index "i_c" for each compound
+		sum = sum + 1
+		i_c = as.integer( (sum-1)/3 ) + 1
+		j_c = (j-1)%%3 + 1
+		name_c = paste('Comp', as.character(i_c), sep = "_")
+		compounds[i_c,1] = name_c
+		compounds[i_c,j_c+1] = data[i,j]
+	}
+}
+
+# Calculate the average for each compound and store in a vector
+
+means = apply(compounds[,2:4], 1, mean)
+sds = apply(compounds[,2:4], 1, sd)
